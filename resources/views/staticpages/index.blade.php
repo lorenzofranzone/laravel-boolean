ee55edb76f666466d1ec95d0eee8856ed2f7e022
Lorenzo <lorenzo.franzone@gmail.com>
2020-03-17 22:06:10 +0100

    css numbers

--- resources/views/staticpages/index.blade.php
+++ resources/views/staticpages/index.blade.php
@@ -11,4 +11,23 @@
    
 </div>
 
+<div class="numbers">
+
+   <div class="container">
+      <div class="box">
+         <div class="title">62.000</div>
+         <p>Le posizioni di lavoro aperte in ambito ICT in Italia nel triennio 2019-2021.</p>
+      </div>
+      <div class="box">
+         <div class="title">49,1%</div>
+         <p>La percentuale delle posizioni aperte sopracitate dedicata agli sviluppatori.</p>
+      </div>
+      <div class="box">
+         <div class="title">12</div>
+         <p>I giorni che mediamente trascorrono dalla fine delle lezioni al primo colloquio.</p>
+      </div>
+   </div>
+   
+</div>
+
 @endsection
